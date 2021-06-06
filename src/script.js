3615963ee4db0e00ec65fc3fb56bd49357fb5b2d
louiseuna <louiseuna15@gmail.com>
2021-06-06 21:22:18 +0100

    Start of forecast unit changes

--- src/script.js
+++ src/script.js
@@ -209,8 +209,14 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(changeForecastUnit);
 }
 
+function changeForecastUnit(coordinates) {
+  console.log(coordinates);
+  let maxTemp = Math.floor(coordinates.data.daily[0].temp.max);
+  console.log(maxTemp);
+}
 //Interchanging temperature units
 function farenheit(event) {
   event.preventDefault();
